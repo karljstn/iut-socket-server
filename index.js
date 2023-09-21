9cca78203584c874d54e0e7f4c956303d6101f5c
karljstn <karljustiniano@gmail.com>
2023-09-21 16:24:03 +0200

    fix: :bug: add vercel prod to cors

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ const randomId = () => crypto.randomBytes(8).toString("hex");
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://multiplayer-test-eta.vercel.app/",
+    ],
   },
 });
 
